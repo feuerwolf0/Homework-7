694bed7e09a1bcb073e79bb265918af0a95a4055
Poletaev Kirill <poletaev777@gmail.com>
2023-03-11 21:37:34 +0300

    Выполнил задание 2

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+def get_shop_list_by_dishes(dishes, person_count):
+    # Получаю рецепты
+    cook_book = get_cook_book('recipes.txt')
+    # Создаю шоп-лист словарь
+    shop_list = {}
+    for dish in dishes:
+        for ingr in cook_book[dish]:
+            ingredient_name, quantity, measure = ingr.values()
+            # Если ингридиент уже есть, увеличиваю его кол-во
+            if ingredient_name in shop_list:
+                shop_list[ingredient_name]['quantity'] += int(quantity)*person_count
+            else:
+                shop_list[ingredient_name] = {'measure': measure, 'quantity': int(quantity)*person_count}
+    return shop_list
+    
+
 def get_cook_book(filename):
     cook_book = {}
     # Открываю файл
@@ -6,9 +22,9 @@ def get_cook_book(filename):
         for i in file.read().split('\n\n'):
             # Ключ наименования блюда
             cook_book[i.split('\n')[0]] = []
-            ingredients = {}
             # Цикл по всем ингридиентам
             for line in range(int(i.split('\n')[1])):
+                ingredients = {}
                 ingredient_name, quantity, measure = i.split('\n')[line+2].split('|')
                 ingredients['ingredient_name'] = ingredient_name.strip()
                 ingredients['quantity'] = quantity.strip()
@@ -19,8 +35,7 @@ def get_cook_book(filename):
 
 
 def main():
-    print(get_cook_book('recipes.txt'))
-
+    print(get_shop_list_by_dishes(['Омлет', 'Запеченный картофель'], 2))
 
 if __name__ == "__main__":
     main()
