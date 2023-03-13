74175d780cc2a19a27ddcc5fe2e4b6274ee804dd
Poletaev Kirill <poletaev777@gmail.com>
2023-03-13 22:38:35 +0300

    Оптимизировал, убрав двойное открытие файлов

--- task_3/main.py
+++ task_3/main.py
@@ -2,30 +2,32 @@ import os
 
 def main():
     count_rows = {}
+    txt_content = {}
     sorted_count_rows = []
     # Указываю директорию где хранятся файлы
-    directory = 'task_3/files/'
+    directory = 'files/'
     # Создаю список файлов в директории
     files = [file for file in os.listdir(directory) if os.path.isfile(f'{directory}/{file}')]
     for file in files:
-        txt = open(f'{directory}/{file}','r', encoding='UTF=8')
-        # Считаю кол-во строк в файле и создаю словарь вида filename: кол-во строк
-        count_rows[file] = len(txt.readlines())
-        txt.close()
+        with open(f'{directory}/{file}','r', encoding='UTF=8') as txt:
+            # Считаю кол-во строк в файле и создаю словарь вида filename: кол-во строк
+            count_rows[file] = len(txt.readlines())
+            # Перемещаю указатель на начало документа
+            txt.seek(0)
+            # Создаю словарь вида filename:content
+            txt_content[file] = txt.readlines()
     # Сортирую получившийся словарь по значениям
     sorted_count_rows = sorted(count_rows,key=count_rows.get)
 
     for file in sorted_count_rows:
-        txt = open(f'{directory}/{file}','rt',encoding="UTF-8")
         # Создаю файл out.txt
-        with open('task_3/out.txt', 'a', encoding="UTF-8") as my_file:
+        with open('out.txt', 'a', encoding="UTF-8") as my_file:
             # Записываю название файла
             my_file.write(file + '\n')
             # Записываю кол-во строк в файле
             my_file.write(str(count_rows[file]) + '\n')
             # Записываю содержимое файла
-            my_file.write(''.join(txt.readlines()) + '\n')
-        txt.close()
+            my_file.write(''.join(txt_content[file]) + '\n')
 
 
 if __name__ == "__main__":
